19b7a4cc5092bc0edc8be86019ead86174aaad4f
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-12 17:23:17 +0530

    userdata

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/PranathiAT/roboshop-ansible roboshop.yml -e role_name = ${name} -e env = ${env} &>>/opt/ansible.log
+
+#ansible-pull -i localhost, -U https://github.com/PranathiAT/roboshop-ansible roboshop.yml -e role_name = ${name} -e env = ${env} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/PranathiAT/roboshop-ansible roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
