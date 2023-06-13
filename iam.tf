d80e078087a3ba09b7ea32c3ef0ec20032a7ca6f
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-13 11:13:53 +0530

    resources in iam

--- iam.tf
+++ iam.tf
@@ -42,10 +42,7 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
             "ssm:GetParameters",
             "ssm:GetParameter"
           ],
-          "Resource": [
-            "arn:aws:ssm:us-east-1:${data.aws_caller_identity.identity.account_id}:parameter/${var.env}.${var.name}.*",
-            var.kms_arn
-           ]
+          "Resource":  concat([var.kms_arn], local.resources)
 
         }
       ]
